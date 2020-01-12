83bcdba9559e391472618759b56a985301a9c052
Zixuan Liu <zixuan.liu1@gmail.com>
2020-01-12 17:12:36 +0100

    Add functionality to print error messages from channel. Add environment variables

--- app/src/main/java/com/example/checknodeavailability/MainActivity.kt
+++ app/src/main/java/com/example/checknodeavailability/MainActivity.kt
@@ -5,18 +5,18 @@ import android.os.Bundle
 import com.jcraft.jsch.JSchException
 import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.JSch
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.AsyncTask
 import android.util.Log
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
 import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
+    val user = System.getenv("username") ?: ""
+    val password = System.getenv("password") ?: ""
+    val host = System.getenv("host") ?: ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,17 +41,49 @@ class MainActivity : AppCompatActivity() {
                 session.setConfig("StrictHostKeyChecking", "no")
                 session.timeout = 10000
                 session.connect()
+
                 val channel = session.openChannel("exec") as ChannelExec
-                val command = "\u0008 \u0013 \u0013 ls"
-                channel.setCommand(command)
-                channel.connect()
+                val command = "ls"
                 channel.setOutputStream(baos)
-                Log.d("MainActivity","Hey ${String(baos.toByteArray())}")
+                channel.setCommand(command)
+                channel.setInputStream(null)
+                channel.setErrStream(System.err)
+                val input: InputStream = channel.inputStream
+                channel.connect()
+
+                Log.d("MA", "successful connect to server")
+                Log.d("MA", "Executing command $command")
+
+                val tmp = ByteArray(1024)
+
+                while(true) {
+                    Log.d("MainActivity", "Pressed true... executing")
+                    Log.d("MainActivity", "input is ${input} and ${input.available()}")
+                    while(input.available() > 0) {
+                        val i = input.read(tmp, 0, 1024)
+                        if (i < 0) {
+                            break
+                        }
+                        Log.d("MainActivity", String(tmp,0, i))
+                    }
+
+                    if (channel.isClosed) {
+                        Log.d("MainActivity", "closed channel")
+                        break
+                    }
+
+                    try {
+                        Log.d("MainActivity", "sleeping...")
+                        Thread.sleep(1000)
+                    } catch (e: Exception) {
+                        Log.d("MainActivity", "$e")
+                    }
+                }
+
                 channel.disconnect()
+                session.disconnect()
             } catch (e: JSchException) {
-                Log.e("MainActivity", "$e")
-
-
+                Log.e("MainActivity", "${e.printStackTrace()}")
             }
             return "Finished"
 
