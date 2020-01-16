235b55052d8e5507313ffd60373e7b36fde87590
Gunnar Lange <lange.gunn@gmail.com>
2020-01-16 00:43:45 +0000

    A rough sketch for the login method

--- app/src/main/java/com/example/checknodeavailability/MainActivity.kt
+++ app/src/main/java/com/example/checknodeavailability/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.checknodeavailability
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jcraft.jsch.JSchException
@@ -8,30 +9,55 @@ import com.jcraft.jsch.JSch
 import android.os.AsyncTask
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
 import java.io.InputStream
 import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
-    val user = System.getenv("username") ?: ""
-    val password = System.getenv("password") ?: ""
-    val host = System.getenv("host") ?: ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val usernameField = findViewById<EditText>(R.id.username)
+        val IPAddressField = findViewById<EditText>(R.id.ipAddress)
+        val passwordField = findViewById<EditText>(R.id.password)
+        val fileOutputStream:FileOutputStream
+
+        val filename = "logininfo.txt"
+
+
+
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            ExecuteSSHCommand().execute()
+            val username = usernameField.getText().toString()
+            val IPAddress = IPAddressField.getText().toString()
+            val password = passwordField.getText().toString()
+            val data = "Username $username\n Password: $password IPAddress: $IPAddress"
+
+            /*
+            try {
+                fileOutputStream = openFileOutput(filename, Context.MODE_PRIVATE)
+                fileOutputStream.write(data.toByteArray())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            */
+
+            val command_output = ExecuteSSHCommand(username, password, IPAddress).execute()
+            Log.d("MA", "$command_output")
+            Log.e("MA", "WORKED!")
         }
 
     }
 
-    private class ExecuteSSHCommand() : AsyncTask<Void, Void, String>()
+    private class ExecuteSSHCommand(var user: String, var password: String, var host: String) : AsyncTask<Void, Void, String>()
     {
+
+
         override fun doInBackground(vararg params: Void?): String? {
-            
             val port = 22
             try {
                 val baos = ByteArrayOutputStream()
@@ -43,7 +69,7 @@ class MainActivity : AppCompatActivity() {
                 session.connect()
 
                 val channel = session.openChannel("exec") as ChannelExec
-                val command = "ls"
+                val command = "rbusy"
                 channel.setOutputStream(baos)
                 channel.setCommand(command)
                 channel.setInputStream(null)
@@ -82,11 +108,12 @@ class MainActivity : AppCompatActivity() {
 
                 channel.disconnect()
                 session.disconnect()
+                return String(tmp)
+
             } catch (e: JSchException) {
                 Log.e("MainActivity", "${e.printStackTrace()}")
+                return "ERROR"
             }
-            return "Finished"
-
         }
 
         override fun onPostExecute(result: String?) {
